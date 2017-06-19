73deaeb2367f55b6991e40417d2a719bbdc56007
shashank <shashank@oneorigin.us>
2017-06-19 19:03:25 +0530

    more_variables_and_names_added

---
+++ more_variables_and_names.py
@@ -0,0 +1,12 @@
+my_name = "shashank shandilya"
+my_age = 24
+my_eye_color = 'brown'
+my_height = 172
+my_weight = 82
+my_hair = 'black'
+
+print("lets talk about %s." % my_name)
+print("he is %d inches tall." % my_height)
+print("my age is %d" % my_age)
+print("he has got %s color hair and %s color eye" % (my_hair, my_eye_color))
+print("if i add %d %d and %d we will get this %d" % (my_age, my_height, my_weight, (my_age + my_height+ my_weight)))
